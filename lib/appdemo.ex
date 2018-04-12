daf2b2329b88956a3f0abd6849f3fec390f425c2
dalongrong <1141591465@qq.com>
2018-04-12 15:28:38 +0800

    add keywords demo

--- lib/appdemo.ex
+++ lib/appdemo.ex
@@ -16,4 +16,16 @@ defmodule Appdemo do
    {:ok,result}= :app.login()
    IO.puts result
   end
+
+  def appdemorong(name: name) do
+    IO.puts "name" <> "#{name}"
+    IO.inspect name
+  end
+  def appdemorong(age: age) do
+    IO.puts "age" <> "#{age}"
+    IO.inspect age
+  end
+  def appdemo do
+    IO.inspect :app.userlogin()
+  end
 end
--- src/app.erl
+++ src/app.erl
@@ -2,9 +2,19 @@
 %%API
 -export([
 login/0,
-login/2
+userlogin/0,
+login/2,
+userlogin/1
 ]).
+-record(user, { name, age }).
 login() ->
     {ok,'dalongdemo'}.
-login(name,password) ->
+
+login(name , password) ->
     {ok,'appdemo'}.
+
+userlogin()->
+    #user{name='dalongdemo',age=44}.
+
+userlogin(user)->
+    user.
